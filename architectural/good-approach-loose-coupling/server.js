b0f5064b6da7f23fb9ef93035b051aa17c97a6e1
Zoltán <b.zolcsi@gmail.com>
2022-10-08 14:34:09 +0200

    Loose coupling first phase

---
+++ architectural/good-approach-loose-coupling/server.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const axios = require('axios');
+const client = axios.create({
+    //baseURL: 'http://' + process.env.KEY_VALUE_STORE_HOST + ':' + process.env.KEY_VALUE_STORE_PORT
+    baseURL: 'http://localhost' + ':' + 7480
+});
+
+app.set('view engine', 'html');
+
+app.engine('html', require('ejs').renderFile);
+
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+/**
+ * Get method
+ * */
+app.get('/', async function (req, res) {
+    res.render('index', {items: mapEntriesToString(await getMap())});
+});
+
+/**
+ * Set method
+ * */
+app.post('/set', async function (req, res) {
+    await sendTo("set", req.body.set_text_key, req.body.set_text_value);
+
+    return res.redirect('/');
+});
+
+/**
+ * Delete method
+ * */
+app.post('/delete', async function (req, res) {
+    await sendTo("delete", req.body.delete_text_key, null);
+
+    return res.redirect('/');
+});
+
+/**
+ * Async function to get the map form the key-store
+ * */
+async function getMap() {
+    let res;
+    await client.get('/get').then(response => res = new Map(response.data)
+    ).catch(error => {
+        console.error(error.message);
+        res = null;
+    })
+
+    return res;
+}
+
+/**
+ * Async function to set key in the key-store
+ * */
+async function sendTo(action, key, value) {
+    await client.post('/action', {
+        "action": action,
+        "key": key,
+        "value": value
+    }).then(response => console.log(response.data)
+    ).catch(error => console.error(error.message));
+}
+
+/**
+ * Converts map to Array
+ * */
+function mapEntriesToString(data) {
+    if (data === null) {
+        return;
+    }
+
+    return Array
+        .from(data.entries(), ([k, v]) => `{${k}:${v}}`)
+        .join(" ; ");
+}
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Node server started");
+});
---
+++ architectural/key-store-loose-coupling/server.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set('view engine', 'html');
+
+app.engine('html', require('ejs').renderFile);
+
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json())
+
+/**
+ * Storage
+ * */
+const map = new Map();
+
+/**
+ * Get method
+ * */
+app.get('/get', (req, res) => {
+    res.end(JSON.stringify(Array.from(map.entries())));
+});
+
+/**
+ * Action method
+ * */
+app.post('/action', function (req, res) {
+    const key = req.body.key;
+    const value = req.body.value;
+    const action = req.body.action;
+
+    if (action === "set") {
+        actionSet(key, value, res);
+
+    } else if (action === "delete") {
+        actionDelete(key, res);
+    }
+});
+
+function actionSet(key, value, res, previous) {
+    if (key === null || value === null) {
+        res.end(mapEntriesToString());
+    }
+
+    if (map.has(key)) {
+        previous = map.get(key);
+    }
+
+    map.set(key, value);
+
+    console.log("(" + key + ") key set: " + previous + "-->" + map.get(key));
+    res.end(mapEntriesToString());
+}
+
+function actionDelete(key, res) {
+    let previous;
+
+    if (map.has(key)) {
+        previous = map.get(key);
+        map.delete(key)
+        console.log("(" + key + ") key delete: " + previous);
+    }
+
+    res.end();
+}
+
+/**
+ * Converts map to Array
+ * */
+function mapEntriesToString() {
+    return Array
+        .from(map.entries(), ([k, v]) => `{${k}:${v}}`)
+        .join(" ; ");
+}
+
+app.listen(process.env.PORT || 7480, () => {
+    console.log("Node server started");
+});
